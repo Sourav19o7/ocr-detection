d1fe0b306727d8f84a5025583f5143a8a537c529
sourav <sourav.dey0147@gmail.com>
2026-04-21 00:41:39 +0530

    chnages

--- src/storage_service.py
+++ src/storage_service.py
@@ -57,6 +57,7 @@ class StorageService:
                 aws_access_key_id=self.config.access_key_id,
                 aws_secret_access_key=self.config.secret_access_key,
                 region_name=self.config.region,
+                endpoint_url=f"https://s3.{self.config.region}.amazonaws.com",
                 config=BotocoreConfig(signature_version="s3v4"),
             )
         except Exception as e:
